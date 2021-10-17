451d6a41e4a37ef63f7e8fe10cb7772da4e68551
Simey de Klerk <13088589+simeydk@users.noreply.github.com>
2021-10-17 06:24:49 +0000

    custom UA WIP

--- checklinks.js
+++ checklinks.js
@@ -5,13 +5,18 @@ import { glob, readFileString} from './utils/files.js';
 import { asyncForEach, asyncMap} from './utils/asyncArray.js'
 import { safeFetch } from './utils/safeFetch.js';
 
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    Array.from(process.argv).forEach((arg, i) => console.log(`${i} - ${arg}`) )
 main()
+}
 
 async function main() {
     const filenames = await glob('**/*.md')
     const links = await asyncMap(filenames, getLinks).then(arr => arr.flat())
-    const brokenLinks = links.filter(x => x.status !== 200)
-    console.log(brokenLinks)
+    const brokenLinks = links.filter(x => !x.ok)
+    console.log(brokenLinks.map(({filename, status, link}) => `${status} ${filename}  ${link}`).join('\n'))
+    console.log(brokenLinks.length , links.length )
 }
 
 async function getLinks(filename) {
--- utils/safeFetch.js
+++ utils/safeFetch.js
@@ -3,9 +3,13 @@ import fetch from 'node-fetch';
 export async function safeFetch(url) {
     let response;
     try {
-        response = await fetch(url);
+        response = await fetch(
+            url,
+            { headers: new Headers({ 
+                "User-Agent": "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0" }) 
+            });
     } catch (e) {
-        response = { status: '' };
+        response = { status: -1, ok: false };
     }
     return response;
 }
