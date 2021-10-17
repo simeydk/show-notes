cabb669a26dde1adabbd70fbeadbcfe1d1d0b5d8
Simey de Klerk <simeydeklerk@gmail.com>
2021-10-17 21:02:37 +0200

    change chunk size to 50

--- src/checklinks.js
+++ src/checklinks.js
@@ -10,7 +10,10 @@ if (fileURLToPath(import.meta.url).startsWith(process.argv[1])) {
     main(pattern)
 }
 
+const CHUNK_SIZE = 50;
+
 const cachedCheckLink = await withLowCache(checkLink, 'linkcache.json', 24 * 60 * 60 * 1000)
+
 
 async function main(globPattern = '**/*.md') {
 
@@ -21,7 +24,7 @@ async function main(globPattern = '**/*.md') {
     console.log(filenames.slice(0,20).join('\n') + (numFiles > 20 ? `\n(...${numFiles - 20} more)` : '') + '\n')
     const links = await asyncMap(filenames, getLinksFromMdFile).then(arr => arr.flat())
     console.log(`Found ${links.length} links`)
-    await asyncChunkedForEach(links, async link => Object.assign(link, await cachedCheckLink(link.url)), 20)
+    await asyncChunkedForEach(links, async link => Object.assign(link, await cachedCheckLink(link.url)), CHUNK_SIZE)
     const brokenLinks = links.filter(x => !x.ok)
     console.log(brokenLinks.map(({filename, status, url}) => `${status} ${filename}  ${url}`).join('\n'))
     console.log(brokenLinks.length , links.length )
