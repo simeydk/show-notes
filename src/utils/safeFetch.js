50f1956006eb2f3950568533bb479e0b21a486ed
Simey de Klerk <simeydeklerk@gmail.com>
2021-10-17 20:56:42 +0200

    add timeout to fetch

--- src/utils/safeFetch.js
+++ src/utils/safeFetch.js
@@ -1,14 +1,22 @@
 import {fileURLToPath} from 'url'
 
 import fetch from 'node-fetch';
+import AbortController from 'abort-controller'
 
-export async function safeFetch(url, ...args) {
+export async function safeFetch(url, options ={}, ...args) {
+    const controller = new AbortController()
+    const timeout = 10000
+    const timer = setTimeout(() => controller.abort(), timeout)
     let response;
     try {
-        response = await fetch(url,...args)
+        const start = Date.now()
+        response = await fetch(url, {...options, signal: controller.signal}, ...args)
+        const t = Date.now() - start
+        response.timeTaken = t
     } catch (e) {
         response = { status: -1, ok: false };
     }
+    clearTimeout(timer)
     return response;
 }
 
