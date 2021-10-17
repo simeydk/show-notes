18dca1a9a95c1c098c257e57fd529d90b554aa7f
Simey de Klerk <13088589+simeydk@users.noreply.github.com>
2021-10-17 06:25:35 +0000

    add import for fileURLToPath

--- checklinks.js
+++ checklinks.js
@@ -1,4 +1,4 @@
-import { URL } from 'url';
+import { URL, fileURLToPath } from 'url';
 
 import { getMarkdownLinksWithLineNumber } from './utils/markdown.js';
 import { glob, readFileString} from './utils/files.js';
